95af6f270f7bad804ab35c3d25b3a06eb72dc649
Piskaeva_Anna <a.g.piskaeva@samsmu.ru>
2024-06-20 18:46:02 +0400

    AddTestAddSectionInCategory

---
+++ src/test/java/admin/utils/preparationDataTests/services/AddDeleteCategorySectionDecorator.java
@@ -0,0 +1,38 @@
+package admin.utils.preparationDataTests.services;
+
+import admin.data.DataConfig;
+import admin.utils.APIUtils.PreparationDataServicesTest;
+import admin.utils.dbUtils.DataBaseQuery;
+import lombok.Getter;
+import lombok.Setter;
+import org.junit.jupiter.api.extension.AfterEachCallback;
+import org.junit.jupiter.api.extension.BeforeEachCallback;
+import org.junit.jupiter.api.extension.ExtensionContext;
+
+import java.util.UUID;
+
+public class AddDeleteCategorySectionDecorator implements BeforeEachCallback, AfterEachCallback {
+
+    @Setter
+    @Getter
+    public static UUID categoryId;
+    @Setter
+    public static UUID sectionId;
+
+
+    @Override
+    public void beforeEach(ExtensionContext context) throws Exception {
+        PreparationDataServicesTest.addCategory(DataConfig.DataTest.getNAME_CATEGORY());
+        UUID categoryId= DataBaseQuery.selectServicesInfo(DataConfig.DataTest.getNAME_CATEGORY()).getId();
+        setCategoryId(categoryId);
+    }
+
+    @Override
+    public void afterEach(ExtensionContext context) throws Exception {
+        UUID sectionId=DataBaseQuery.selectServicesInfo(DataConfig.DataTest.getNAME_SECTION()).getId();
+        setSectionId(sectionId);
+        PreparationDataServicesTest.deleteCategory(sectionId);
+        PreparationDataServicesTest.deleteCategory(categoryId);
+    }
+
+}
